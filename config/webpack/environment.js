bf3c1e45c41f4960d5cb2273e3159022f8c0aa95
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-01-05 23:38:13 +0530

    babel resolved

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,21 @@
-const { environment } = require('@rails/webpacker')
+// webpack environment.js
 
-module.exports = environment
+const { environment } = require('@rails/webpacker');
+
+// Get the Babel loader from the environment
+const babelLoader = {
+  test: /\.m?js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+      // Add other Babel options or plugins here if needed
+    }
+  }
+};
+
+// Append Babel loader to the environment
+environment.loaders.append('babel', babelLoader);
+
+module.exports = environment;
